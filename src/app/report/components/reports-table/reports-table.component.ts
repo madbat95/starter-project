709bba2439a11a435fc2e158bf150685e6d9a7bc
Hammad <hamahmed95@gmail.com>
2023-07-28 01:42:04 +0500

    added styling to second card table

--- src/app/report/components/reports-table/reports-table.component.ts
+++ src/app/report/components/reports-table/reports-table.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-reports-table',
   templateUrl: './reports-table.component.html',
-  styleUrls: ['./reports-table.component.scss']
+  styleUrls: ['./reports-table.component.scss'],
 })
 export class ReportsTableComponent {
-
+  reportsData: any[] = [
+    {
+      reportKeyword: 'kitchen remodeling los angeles',
+      country: 'US',
+      language: 'EN',
+      created: 'an hour ago',
+      wordCount: 0,
+      contentGrade: '1/10',
+      user: 'Saad Wakeel',
+    },
+    {
+      reportKeyword: 'personal injury attorney los angeles',
+      country: 'US',
+      language: 'EN',
+      created: 'an hour ago',
+      wordCount: 0,
+      contentGrade: '1/10',
+      user: 'Saad Wakeel',
+    },
+    {
+      reportKeyword: 'public adjuster los angeles',
+      country: 'US',
+      language: 'EN',
+      created: 'an hour ago',
+      wordCount: 0,
+      contentGrade: '1/10',
+      user: 'Saad Wakeel',
+    },
+  ];
 }
