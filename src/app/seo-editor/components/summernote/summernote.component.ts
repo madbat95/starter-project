31ee2ed4ab9e711f2cc15d5a7d5f8b7cbb16046c
Hammad <hamahmed95@gmail.com>
2023-07-28 21:27:44 +0500

    added word counter in summernote.

--- src/app/seo-editor/components/summernote/summernote.component.ts
+++ src/app/seo-editor/components/summernote/summernote.component.ts
@@ -55,17 +55,28 @@ export class SummernoteComponent implements OnInit {
     this.wordCountData = {}; // Reset the word count data
     this.uniqueWords.clear(); // Clear the uniqueWords Set
 
-    const words = text.split(/\s+/).filter(Boolean);
+    // Remove HTML entities representing spaces (&nbsp;), <p> tags, and <br> tags from the text
+    const cleanedText = text.replace(/(&nbsp;|<p>|<\/p>|<br>)/g, '');
 
-    words.forEach((word) => {
-      // Use a Set to keep track of unique words and only count them once
-      if (!this.uniqueWords.has(word)) {
-        this.uniqueWords.add(word);
-        this.wordCountData[word] = 1;
-      } else {
-        this.wordCountData[word]++;
+    const lines = cleanedText.split('\n');
+
+    lines.forEach((line) => {
+      // Check if the line is not empty and not containing only spaces
+      if (line.trim().length > 0) {
+        const words = line.trim().split(/\s+/);
+
+        words.forEach((word) => {
+          // Use a Set to keep track of unique words and only count them once
+          if (!this.uniqueWords.has(word)) {
+            this.uniqueWords.add(word);
+            this.wordCountData[word] = 1;
+          } else {
+            this.wordCountData[word]++;
+          }
+        });
       }
     });
+
     console.log('Word Count Data:', this.wordCountData);
   }
 }
