d925f5ef0daefb67d3de2913e3619e8950dd98ff
Hammad <hamahmed95@gmail.com>
2023-08-03 16:35:44 +0500

    updated function for calculating number of words

--- src/app/seo-editor/components/summernote/summernote.component.ts
+++ src/app/seo-editor/components/summernote/summernote.component.ts
@@ -49,17 +49,17 @@ export class SummernoteComponent implements OnInit {
     ],
   };
 
-  constructor(private wordCountService: WordCountService) {}
-
   ngOnInit(): void {}
 
   getAllHeadings(html) {
     const parser = new DOMParser();
     const doc = parser.parseFromString(html, 'text/html');
-
+    // console.log('doc here', doc);
     const headingCounts = {};
-
-    ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].forEach((tagName) => {
+    // Initialize an array to hold content from non-heading tags
+    let content = [];
+    const heading = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+    heading.forEach((tagName) => {
       const headings = doc.querySelectorAll(tagName);
       const headingTexts = Array.from(headings).map(
         (heading: any) => heading.innerText
@@ -69,46 +69,118 @@ export class SummernoteComponent implements OnInit {
         const words = headingText.trim().split(/\s+/);
         wordCount += words.length;
       });
-
       headingCounts[tagName] = wordCount;
     });
-
+    // Iterate through other tags and collect their content
+    const nonHeadingTags = ['p', 'div', 'span', 'a']; // Add more tags as needed
+    nonHeadingTags.forEach((tagName) => {
+      const elements = doc.querySelectorAll(tagName);
+      elements.forEach((element: any) => {
+        content.push(element.innerText);
+      });
+    });
+    // Join the content array and split into words to count
+    const contentText = content.join(' ');
+    const contentWords = contentText.trim().split(/\s+/);
+    headingCounts['content'] = contentWords.length;
     console.log('Heading Word Count:', headingCounts);
     return headingCounts;
   }
 
+  // iterateBodyElements(element, resultMap = {}) {
+  //   var tagName = element.tagName.toLowerCase();
+  //   var innerText = element.innerText.trim();
+  //   var wordCount = innerText.split(/\s+/).length;
+
+  //   if (tagName.startsWith('h') && tagName.length === 2 && !isNaN(tagName[1])) {
+  //     resultMap[tagName] = wordCount;
+  //   } else {
+  //     if ('content' in resultMap) {
+  //       resultMap['content'] += wordCount;
+  //       console.log('wordCount :', wordCount);
+  //       console.log('resultmap: ', resultMap['content']);
+  //     } else {
+  //       resultMap['content'] = wordCount;
+  //     }
+  //   }
+
+  //   var children = element.children;
+
+  //   for (var i = 0; i < children.length; i++) {
+  //     this.iterateBodyElements(children[i], resultMap);
+  //   }
+
+  //   return resultMap;
+  // }
+  iterateBodyElements(element, resultMap = {}) {
+    var children = element.children;
+    console.log('children', children);
+    for (let i = 0; i < children.length; i++) {
+      let tagName = children[i].tagName.toLowerCase();
+      // console.log('eklement', element);
+      if (
+        tagName.startsWith('h') &&
+        tagName.length === 2 &&
+        !isNaN(tagName[1])
+      ) {
+        var innerText = children[i].innerText.trim();
+        var wordCount = innerText.split(/\s+/).length;
+        resultMap[tagName] = (resultMap[tagName] || 0) + wordCount; // Increment the count for the heading tag
+      } else {
+        // If it's not a heading tag, process the text content without counting child elements
+        const cleanedText = children[i].innerText.replace(/\s+/g, ' ').trim();
+
+        if (cleanedText !== '') {
+          // Only count non-empty text
+          console.log('cleanedText', cleanedText);
+          let wordCount = cleanedText.split(/\s+/).length;
+          console.log('word count', wordCount);
+          resultMap['content'] = (resultMap['content'] || 0) + wordCount; // Increment the content count
+
+          // console.log('Words in content:', cleanedText.split(/\s+/));
+        }
+      }
+      this.iterateBodyElements(children[i], resultMap);
+    }
+
+    return resultMap;
+  }
+
   onEditorKeyUp(text: any) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(text, 'text/html');
     this.wordCountData = {}; // Reset the word count data
     this.uniqueWords.clear(); // Clear the uniqueWords Set
-
-    console.log('text entered', text);
+    // console.log('text: ', text);
+    console.log('doc body: ', doc.body);
+    // console.log('text entered', text);
 
     // Count words in headings
     const headingCounts = this.getAllHeadings(text);
+    // console.log();
+    var c = this.iterateBodyElements(doc.body);
+    // console.log('final', c);
 
     // Remove HTML entities representing spaces (&nbsp;), <p> tags, and <br> tags from the text
-    const cleanedText = text.replace(/(&nbsp;|<p>|<\/p>|<br>)/g, '');
+    const cleanedText = text.replace(/<\/?[^>]+(>|$)/g, ' ');
 
-    const lines = cleanedText.split('\n');
+    const words = cleanedText.split(/\s+/);
 
-    lines.forEach((line) => {
-      // Check if the line is not empty and not containing only spaces
-      if (line.trim().length > 0) {
-        const words = line.trim().split(/\s+/);
-
-        words.forEach((word) => {
-          // Use a Set to keep track of unique words and only count them once
-          if (!this.uniqueWords.has(word)) {
-            this.uniqueWords.add(word);
-            this.wordCountData[word] = 1;
-          } else {
-            this.wordCountData[word]++;
-          }
-        });
+    words.forEach((word) => {
+      // Check if the word is not empty and not containing only spaces
+      if (word.trim().length > 0) {
+        // Use a Set to keep track of unique words and only count them once
+        if (!this.uniqueWords.has(word)) {
+          this.uniqueWords.add(word);
+          this.wordCountData[word] = 1;
+        } else {
+          this.wordCountData[word]++;
+        }
       }
     });
 
     console.log('Word Count Data:', this.wordCountData);
+
     this.onWordCount.emit(this.wordCountData);
   }
 }
