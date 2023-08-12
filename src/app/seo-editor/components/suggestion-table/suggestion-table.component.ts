a5b4414cc3287e8199ce0e3df0c8378f7b509b22
Hammad <hamahmed95@gmail.com>
2023-08-12 20:17:47 +0500

    used renderer to creata a blurtoggle effect on selective elements of suggestion table's meta column

--- src/app/seo-editor/components/suggestion-table/suggestion-table.component.ts
+++ src/app/seo-editor/components/suggestion-table/suggestion-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
 import { WordCounterService } from '../../service/word-counter.service';
 
 @Component({
@@ -8,12 +8,39 @@ import { WordCounterService } from '../../service/word-counter.service';
 })
 export class SuggestionTableComponent implements OnInit {
   tableData: any;
-  // @Input() set data(val: any) {
-  //   this.tableData = val;
-  // }
-  constructor(private wordCounter: WordCounterService) {}
+  blurState: boolean = false; // Keep track of blur state
+  cellIdsToBlur: string[] = [
+    'titleCount',
+    'entityTitleCount',
+    'lsiTitleCount',
+    'variationTitleCount',
+  ];
+
+  constructor(
+    private wordCounter: WordCounterService,
+    private renderer: Renderer2,
+    private el: ElementRef
+  ) {}
+
   ngOnInit(): void {
     this.tableData = this.wordCounter.wordCount;
     console.log(this.tableData);
+  }
+
+  titleBlur(): void {
+    console.log('button pressed');
+
+    this.blurState = !this.blurState;
+
+    this.cellIdsToBlur.forEach((id) => {
+      const element = this.el.nativeElement.querySelector(`#${id}`);
+      if (element) {
+        if (this.blurState) {
+          this.renderer.setStyle(element, 'filter', 'blur(5px)');
+        } else {
+          this.renderer.removeStyle(element, 'filter');
+        }
+      }
+    });
   }
 }
