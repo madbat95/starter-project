e2df0d881eeb134969e2efaf429852eb65cbf3ec
Hammad <hamahmed95@gmail.com>
2023-08-15 16:43:10 +0500

    added old code back without toggle

--- src/app/shared/directives/table-blur.directive.ts
+++ src/app/shared/directives/table-blur.directive.ts
@@ -7,24 +7,43 @@ export class TableBlurDirective {
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   @HostListener('click') onClick() {
-    console.log('on click directive');
-    const columnCells =
-      this.el.nativeElement.parentElement.querySelectorAll('td');
-    const columnIndex = Array.from(columnCells).indexOf(this.el.nativeElement);
+    console.log('on click');
 
     const table = this.el.nativeElement.closest('table');
-    const allCells = table.querySelectorAll('td');
+    const columnIndex = this.getColumnIndex(this.el.nativeElement);
+    console.log('columnIndex', columnIndex);
+
+    const allCells = table.querySelectorAll('td, th');
 
     for (const cell of allCells) {
+      console.log('cell', cell.tagName);
+      const cellIndex = this.getColumnIndex(cell);
       if (
-        Array.from(allCells).indexOf(cell) % columnCells.length ===
-        columnIndex
+        cellIndex === columnIndex ||
+        (cellIndex === columnIndex + 1 && cell.tagName == 'TD') ||
+        (cellIndex === columnIndex - 1 && cell.tagName == 'TH')
       ) {
-        // this.renderer.addClass(cell, 'focused');
-        this.renderer.removeClass(cell, 'focused');
+        if (cell.classList.contains('focused')) {
+          this.renderer.removeClass(cell, 'focused');
+        }
       } else {
         this.renderer.addClass(cell, 'focused');
       }
     }
+  }
+
+  private getColumnIndex(cell: any): number {
+    const row = cell.parentElement;
+    let cellIndex = 0;
+    // console.log(' row.cells', row.cells);
+    for (let i = 0; i < row.cells.length; i++) {
+      const currentCell = row.cells[i];
+      if (currentCell === cell) {
+        return cellIndex;
+      }
+      cellIndex += currentCell.colSpan || 1;
+    }
+
+    return -1;
   }
 }
