61a767366351f11d27e0332bcb6aece14b4ab4f7
Areeb Raza <rydaholic@gmail.com>
2023-08-19 17:20:39 +0500

    Modify request headers for scraping api

--- src/app/shared/services/scrape.service.ts
+++ src/app/shared/services/scrape.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ScrapeService {
-  constructor(private http: HttpClient) {}
 
+  constructor(private http: HttpClient) {}
+  
   getHTML(url: any) {
-    return this.http.get(`reporting/scrap/?site_url=${url}`);
+    const headers = new HttpHeaders({ 'Content-Type': 'text/html' });
+    return this.http.get(`reporting/scrap/?site_url=${url}`, { headers, responseType: 'text' });
   }
 }
