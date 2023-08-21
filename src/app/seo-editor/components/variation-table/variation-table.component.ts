a580787ad011370ee60f7ba1d6deb5fc8e916d5f
Anas Khalid <anas.khalid@mygoldcare.com>
2023-08-21 18:32:15 +0500

    virtual-scrolling

--- src/app/seo-editor/components/variation-table/variation-table.component.ts
+++ src/app/seo-editor/components/variation-table/variation-table.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, AfterViewInit } from '@angular/core';
+import { NzTableComponent } from 'ng-zorro-antd/table';
 import { TableLoaderService } from 'src/app/shared/services/table-loader.service';
 
 @Component({
@@ -6,16 +7,27 @@ import { TableLoaderService } from 'src/app/shared/services/table-loader.service
   templateUrl: './variation-table.component.html',
   styleUrls: ['./variation-table.component.scss'],
 })
-export class VariationTableComponent {
+export class VariationTableComponent implements AfterViewInit {
+  @ViewChild('virtualTable', { static: false }) nzTableComponent?: NzTableComponent<{ word: string; count: { meta: number, summer_note: number }, index: number }[]>;
   constructor(public tableLoader: TableLoaderService) {}
 
   searchValue = '';
   visible = false;
-  tableData: { word: string; count: number }[] = [];
+  tableData: { word: string; count: { meta: number, summer_note: number }, index: number }[] = [
+  ];
   originalData: any;
-  @Input() set data(val: { word: string; count: number }[]) {
+  @Input() set data(val: { word: string; count: { meta: number, summer_note: number } }[]) {
     console.log('val', val);
-    this.tableData = val;
+    this.tableData = [];
+    this.tableLoader.variationTableLoader = true
+    for (let i = 0; i < val.length; i++) {
+      this.tableData.push({
+        index: i,
+        ...val[i]
+      });
+    }
+    this.tableLoader.variationTableLoader = false
+    console.log('table data',this.tableData)
     this.originalData = [...this.tableData];
   }
   reset(): void {
@@ -32,5 +44,21 @@ export class VariationTableComponent {
 
   deleteRow(word: string): void {
     this.tableData = this.tableData.filter((deleted) => deleted.word !== word);
+  }
+
+  scrollToIndex(index: number): void {
+    this.nzTableComponent?.cdkVirtualScrollViewport?.scrollToIndex(index);
+  }
+
+  trackByIndex(_: number, data: any): number {
+    return data.index
+  }
+
+  ngAfterViewInit(): void {
+    this.nzTableComponent?.cdkVirtualScrollViewport?.scrolledIndexChange
+      // .pipe(takeUntil(this.destroy$))
+      .subscribe((data: number) => {
+        console.log('scroll index to', data);
+      });
   }
 }
