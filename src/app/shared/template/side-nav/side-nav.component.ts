1bcc590fcb4a65754603a2541482eaadec46d6a2
Hammad <hamahmed95@gmail.com>
2023-08-31 16:36:48 +0500

    removed contempo

--- src/app/shared/template/side-nav/side-nav.component.ts
+++ src/app/shared/template/side-nav/side-nav.component.ts
@@ -3,32 +3,36 @@ import { ROUTES } from './side-nav-routes.config';
 import { ThemeConstantService } from '../../services/theme-constant.service';
 
 @Component({
-    selector: 'app-sidenav',
-    templateUrl: './side-nav.component.html'
+  selector: 'app-sidenav',
+  templateUrl: './side-nav.component.html',
 })
+export class SideNavComponent {
+  public menuItems: any[];
+  isFolded: boolean;
+  isSideNavDark: boolean;
+  isExpand: boolean;
 
-export class SideNavComponent{
+  constructor(private themeService: ThemeConstantService) {}
 
-    public menuItems: any[]
-    isFolded : boolean;
-    isSideNavDark : boolean;
-    isExpand : boolean;
+  ngOnInit(): void {
+    this.menuItems = ROUTES.filter((menuItem) => menuItem);
+    this.themeService.isMenuFoldedChanges.subscribe(
+      (isFolded) => (this.isFolded = isFolded)
+    );
+    this.themeService.isExpandChanges.subscribe(
+      (isExpand) => (this.isExpand = isExpand)
+    );
+    this.themeService.isSideNavDarkChanges.subscribe(
+      (isDark) => (this.isSideNavDark = isDark)
+    );
+  }
 
-    constructor( private themeService: ThemeConstantService) {}
-
-    ngOnInit(): void {
-        this.menuItems = ROUTES.filter(menuItem => menuItem);
-        this.themeService.isMenuFoldedChanges.subscribe(isFolded => this.isFolded = isFolded);
-        this.themeService.isExpandChanges.subscribe(isExpand => this.isExpand = isExpand);
-        this.themeService.isSideNavDarkChanges.subscribe(isDark => this.isSideNavDark = isDark);
+  closeMobileMenu(): void {
+    if (window.innerWidth < 992) {
+      this.isFolded = false;
+      this.isExpand = !this.isExpand;
+      this.themeService.toggleExpand(this.isExpand);
+      this.themeService.toggleFold(this.isFolded);
     }
-
-    closeMobileMenu(): void {
-        if (window.innerWidth < 992) {
-            this.isFolded = false;
-            this.isExpand = !this.isExpand;
-            this.themeService.toggleExpand(this.isExpand);
-            this.themeService.toggleFold(this.isFolded);
-        }
-    }
+  }
 }
