cf1f2c7f10f8b079d2b1d27f7f0ee2d03eb581c6
Hammad <hamahmed95@gmail.com>
2023-09-05 17:15:09 +0500

    added confirm password validator in reset password screen

--- src/app/auth/components/password-reset/password-reset.component.ts
+++ src/app/auth/components/password-reset/password-reset.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  UntypedFormControl,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { AuthService } from 'src/app/shared/services/auth.service';
@@ -32,27 +37,55 @@ export class PasswordResetComponent implements OnInit {
     });
     this.newPasswordForm = this.fb.group({
       password: [null, [Validators.required]],
+      checkPassword: [null, [Validators.required, this.confirmationValidator]],
     });
   }
 
-  onSubmit(): void {
-    this.loading = true;
-    const requestBody = {
-      uid: this.uid,
-      token: this.token,
-      new_password: this.newPasswordForm.get('password')?.value,
-    };
+  confirmationValidator = (
+    control: UntypedFormControl
+  ): { [s: string]: boolean } => {
+    if (!control.value) {
+      return { required: true };
+    } else if (control.value !== this.newPasswordForm.controls.password.value) {
+      return { confirm: true, error: true };
+    }
+    return {};
+  };
 
-    this.authService.resetAccount(requestBody).subscribe({
-      next: (response: any) => {
-        this.message.success('Password updated successfully');
-        this.router.navigate(['/auth/login']);
-        this.loading = false;
-      },
-      error: (error: any) => {
-        this.message.error('Password reset failed');
-        this.loading = false;
-      },
-    });
+  updateConfirmValidator(): void {
+    /** wait for refresh value */
+    Promise.resolve().then(() =>
+      this.newPasswordForm.controls.checkPassword.updateValueAndValidity()
+    );
+  }
+
+  onSubmit(): void {
+    if (this.newPasswordForm.valid) {
+      this.loading = true;
+      const requestBody = {
+        uid: this.uid,
+        token: this.token,
+        new_password: this.newPasswordForm.get('password')?.value,
+      };
+
+      this.authService.resetAccount(requestBody).subscribe({
+        next: (response: any) => {
+          this.message.success('Password updated successfully');
+          this.router.navigate(['/auth/login']);
+          this.loading = false;
+        },
+        error: (error: any) => {
+          this.message.error('Password reset failed');
+          this.loading = false;
+        },
+      });
+    } else {
+      Object.values(this.newPasswordForm.controls).forEach((control) => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
   }
 }
