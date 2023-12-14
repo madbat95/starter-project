cd8ac23d83c4e4357a9ba47874d1700d2c775cef
Hammad <hamahmed95@gmail.com>
2023-12-15 01:05:19 +0500

    added company table and routing method

--- src/app/company/components/company-table/company-table.component.ts
+++ src/app/company/components/company-table/company-table.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-company-table',
   templateUrl: './company-table.component.html',
-  styleUrls: ['./company-table.component.scss']
+  styleUrls: ['./company-table.component.scss'],
 })
 export class CompanyTableComponent {
+  constructor(private router: Router, private route: ActivatedRoute) {}
+  data = [
+    {
+      memberName: 'John Malkovich',
+      beneficialOwner: 'Yes',
+      roles: 'Account Manager, Account Supervisor, Billing',
+      actions: 'Action',
+    },
+  ];
 
+  addCompany() {
+    this.router.navigate(['companyinfo'], {
+      relativeTo: this.route,
+      queryParams: { tab: 'estimates' },
+    });
+  }
 }
